8a86be068c94651ce99a16270c5da7c122ceda4f
AlexandrosManolis <it2021132@hua.gr>
2024-05-05 19:48:04 +0300

    add new features (damage percentage view)

---
+++ src/views/DamagePercentageView.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+import {useRoute, useRouter} from "vue-router";
+import {computed, ref, onMounted} from "vue";
+import {useRemoteData} from "@/composables/useRemoteData.js";
+import {sharedState} from "@/stores/sharedState.js";
+
+const router = useRouter();
+const route = useRoute();
+const userIdRef = ref(null);
+const declarationIdRef = ref(null);
+
+// State value
+const damagePercentage = sharedState.damagePercentage;
+
+onMounted(() => {
+  userIdRef.value = route.params.userId;
+  declarationIdRef.value = route.params.declarationId;
+  performRequest();
+  // Redirection
+  window.location.href='/users/'+ userIdRef.value +'/user-declarations';
+});
+
+// API URL with userId, declarationId, and amount
+const urlRef = computed(() => {
+  return `http://localhost:9090/api/declaration/report/`+userIdRef.value+`/damageReport/`+declarationIdRef.value+`?damagePercentage=${encodeURIComponent(damagePercentage)}`;
+});
+
+
+const authRef = ref(true);
+const methodRef = ref("POST");
+
+const { data, performRequest } = useRemoteData(urlRef, authRef, methodRef);
+
+
+</script>
+
+<template>
+  <tbody v-if="loading" class="loading-spinner">
+  <tr>
+    <td colspan="5">Loading...</td>
+  </tr>
+  </tbody>
+</template>
+
+
+<style scoped>
+
+.loading-spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
